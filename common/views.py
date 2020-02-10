b15ba6ca5b6cdf61baafc2dcb1f970e1660e8860
jshinv <jshinv@gmail.com>
2020-02-10 16:33:35 +0900

    add images

--- common/views.py
+++ common/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from common.models import *
-from django.db.models import Sum, Count
+from django.db.models import Sum, Count, Value
+from django.db.models.functions import Replace  
+
 
 def index(request):
     product_data = Product.objects.all()
@@ -29,6 +31,16 @@ def price_chart(request):
     labels = []
     data = []
 
+    prd_price = Product.objects.values('prd_price')
+    # prd_price = prd_price.replace("원", "")
+    # prd_price = prd_price.replace(",", "")
+    # prd_price = int(prd_price)
+
+    # Product.objects.update(field=Replace(
+    #     'prd_price', Value('string'), Value('int')
+    # ))
+
+
     queryset = Product.objects.values('prd_id').annotate(price=Sum('prd_price')).order_by('-price')
     for entry in queryset:
         labels.append(entry['prd_id'])
@@ -44,6 +56,7 @@ def review_chart(request):
     # 상품명을 라벨로 둔다
     data = []
     # 리뷰수를 보여준다
+
     queryset = Review.objects.values('prd_id').annotate(review_total=Sum('review_id')).order_by('-review_total')
     
     for entry in queryset:
